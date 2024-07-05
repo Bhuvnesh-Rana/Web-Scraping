caeb3b99d78916c0f657c7a9a6b63103c12dd1b5
Bhuvnesh Rana <168338773+Bhuvnesh-Rana@users.noreply.github.com>
2024-07-05 09:02:07 +0000

    Web Scraping.

---
+++ Random/DemoJavaRun.java
@@ -0,0 +1,9 @@
+/**
+ * DemoJavaRun
+ */
+public class DemoJavaRun {
+
+    public static void main(String[] args) {
+        System.out.println("Happy.");
+    }
+}
---
+++ scraping/src/main/java/com/scraping/App.java
@@ -0,0 +1,30 @@
+package com.scraping;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String url = "https://books.toscrape.com/";
+        System.out.println("*********************************************");
+        System.out.println(":WEB SCRAPING:");
+        System.out.println("*********************************************");
+        System.out.println();
+        try {
+            Document document = Jsoup.connect(url).get();
+            Elements books = document.select(".product_pod");
+
+            for (Element book : books) {
+                String title = book.select("h3 > a").text();
+                String price = book.select(".price_color").text();
+                System.out.println(title + " : " + price);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
